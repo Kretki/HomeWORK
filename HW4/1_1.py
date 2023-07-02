499154b6c6ed24e686a6d37bfb66e2b234266d7f
Kretki <grigoriotvext@mail.ru>
2023-07-02 19:31:42 +0300

    NotWorkingDistinguish

--- HW4/1_1.py
+++ HW4/1_1.py
@@ -13,40 +13,74 @@ def connect_start_and_end(contours):
                 cords = [i[0], j[0]]
     return cords
 
-image = cv2.imread(f'./HW4/images/whale_tail/test_image_00.jpg')
+image1 = cv2.imread(f'./HW4/images/whale_tail/test_image_00.jpg')
 
-gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 gray_img = cv2.GaussianBlur(gray_img, ksize=(7, 7), sigmaX=2, sigmaY=2)
 cv2.line(gray_img, (995, 107), (1007, 107), (0,0,0), thickness=3)
 
 edges = skimage.feature.canny(gray_img, sigma=2)
-# ## выделяем границы
-# laplac = cv2.Laplacian(gray_img, cv2.THRESH_BINARY, scale=0.55, ksize=5)
 edges = edges.astype('uint8')
 contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 new_cont = []
 for cont in contours:
     if cont.shape[0] > 360:
         new_cont.append(cont)
-cv2.drawContours(edges, new_cont, -1, (255,0,0), 3)#, cv2.LINE_AA, hierarchy, 1)
-contours1, hierarchy1 = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+res = np.zeros((500, 1100))#found good
+res = res.astype('uint8')
+cv2.drawContours(res, new_cont, -1, (255,0,0), 3)
+
+h,w = res.shape
+pts1 = np.float32([[24, 102], [474, 412], [1007, 50]])
+pts2 = np.float32([[5, 50], [500, 495], [1085, 5]])#good transformation for _00
+M = cv2.getAffineTransform(pts1, pts2)
+dst = cv2.warpAffine(res, M, (w, h))
+rows, cols = gray_img.shape
+gray_img_1 = np.zeros((500, 1100), dtype=np.uint8)
+gray_img_1[:rows, :cols] = gray_img
+dst1 = cv2.warpAffine(gray_img_1, M, (1100, 500))
+
+
+contours1, hierarchy1 = cv2.findContours(dst, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 new_cont = []
 for cont in contours1:
     if cont.shape[0] > 360:
         new_cont.append(cont)
-res = np.zeros_like(edges)
-M = cv2.moments(new_cont[0])
-print(M)#found good
+#So, now we have good contour from 00 pic
 
-res = res.astype('uint8')
-cv2.drawContours(res, new_cont, -1, (255,0,0), 3)#, cv2.LINE_AA, hierarchy, 1)
-fig, axs = plt.subplots(1, 2, figsize = (10, 4))
-ax1, ax2 = axs
+image2 = cv2.imread(f'./HW4/images/whale_tail/test_image_06.jpg')
+dst2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.GaussianBlur(dst2, ksize=(7, 7), sigmaX=2, sigmaY=2)
+p_l = np.percentile(gray2, 2)
+p_h = np.percentile(gray2, 95)
+gray2_contrast = skimage.exposure.rescale_intensity(gray2, in_range=(p_l, p_h))
 
-ax1.imshow(image.copy())
+edges3 = skimage.feature.canny(gray2_contrast, sigma=0.55)
+edges3 = edges3.astype('uint8')
+contours3, hierarchy3 = cv2.findContours(edges3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+new_cont3 = []
+for cont in contours3:
+    if cont.shape[0] > 50:
+        new_cont3.append(cont)
+res3 = np.zeros((500, 1100))#found good
+res3 = res3.astype('uint8')
+cv2.drawContours(res3, new_cont3, -1, (255,0,0), 3)
+
+d1 = cv2.matchShapes(dst1, dst2, cv2.CONTOURS_MATCH_I1,0)
+d2 = cv2.matchShapes(dst1, dst2, cv2.CONTOURS_MATCH_I2,0)
+d3 = cv2.matchShapes(dst1, dst2, cv2.CONTOURS_MATCH_I3,0)
+print(d1, d2, d3)
+
+fig, axs = plt.subplots(1, 3, figsize = (10, 4))
+ax1, ax2, ax3 = axs
+
+ax1.imshow(image1.copy())
 ax1.set_title('image', fontsize=15)
 
-ax2.imshow(edges.copy(), 'gray')
+ax2.imshow(dst1.copy(), 'gray')
 ax2.set_title('res', fontsize=15)
+
+ax3.imshow(res3.copy(), 'gray')
+ax3.set_title('res2', fontsize=15)
 
 plt.show()
